f3ebd536f68e9d860d68e9644ffc35fdf1eb8601
Just-shuklaastha07 <shuklaastha07@gmail.com>
2026-09-08 10:34:20 +0530

    Getting teams data from EchoAPI

--- Lab3/teams.js
+++ Lab3/teams.js
@@ -22,7 +22,7 @@ export const getAllTeams = () => teams;
 export const getTeamById = (id) => teams.find((team) => team.id === id);
 
 export const addTeam = (newTeam) => {
-    const team = { id: nextId++, newTeam };
+    const team = { id: nextId++, ...newTeam };
     teams.push(team);
     return team;
 };
