db709c32c454ef0bf8b1b2e99c55ac4e70a78aef
Just-shuklaastha07 <shuklaastha07@gmail.com>
2026-09-17 14:01:06 +0530

    Basic routing with express server

--- lab5/app.js
+++ lab5/app.js
@@ -3,7 +3,27 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Express!</h1>");
+  res.send({"message": "Hello Express!"});
+});
+
+app.get("/about", (req, res) => {
+  res.send({"message": "We are FSD Developers"});
+});
+
+app.post("/login", (req, res) => {
+  res.send({"message": "user login"});
+});
+
+app.put("/user/update/1", (req, res) => {
+  res.send({"message": "user update"});
+});
+
+app.delete("/user/1", (req, res) => {
+  res.send({"message": "remove user 1"});
+});
+
+app.use((req, res) => {
+  res.status(404).send({"message": "404 Not Found"});
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
